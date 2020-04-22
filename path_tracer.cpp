59a269f0c0d3127fbd4ea37a00d7bc05bab27d88
mariabrbz <maria.barbulescu@polytechnique.edu>
2020-04-22 16:05:00 +0200

    Antialiasing

--- path_tracer.cpp
+++ path_tracer.cpp
@@ -1,10 +1,10 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "C:\Users\maria\Desktop\Computer Graphics\stb_image_write.h"
 #include "stdlib.h"
-//#include "tools.cpp"
 #include "tools.cpp"
 
 int main() {
+
     // generating the spheres
     Sphere red_sphere(Vector(0, 1000, 0), 940, Vector(1, 0, 0), false, false);
     Sphere green_sphere(Vector(0, 0, -1000), 940, Vector(0, 1, 0), false, false);
@@ -18,7 +18,7 @@ int main() {
     Vector light_source = Vector(-10, 20, 40);
 
     //creating the scene
-    static Sphere A[] = {red_sphere, blue_sphere, green_sphere, pink_sphere, cyan_sphere, yellow_sphere, object};//, object2, object3};
+    static Sphere A[] = {red_sphere, blue_sphere, green_sphere, pink_sphere, cyan_sphere, yellow_sphere, object, object2, object3};
     vector<Sphere> scene_components(A, A + sizeof(A) / sizeof(A[0]));
     Scene scene(scene_components);
 
@@ -26,22 +26,21 @@ int main() {
     Vector Q = Vector(0, 0, 55);                //camera center
     double W = 600;                             //grid width
     double H = 512;                             //grid height
-    double fov = PI/2.5;                        //alpha, field of view
+    double fov = PI/2.5;                          //alpha, field of view
     vector<unsigned char> img(W*H*3);           //image vector
 
-    #pragma omp parallel for schedule(dynamic, 1) 
-    for (int i = 0; i < static_cast<int>(H); i++) {                    //static_cast for parallel programming (since we use int < double)
+    for (int i = 0; i < H; i++) {
         for (int j = 0; j < W; j++) {
+            Vector V;
+            V[2] = Q[2] - (W / (2 * tan(fov / 2))); 
             Vector sum = Vector(0., 0., 0.);
             int limit = 500;
-            Vector V;
-
+            
             for (int k = 0; k < limit; k++) {
-                V[0] = Q[0] + 0.5 + j - (W / 2) + boxMuller()[0];
-                V[1] = Q[1] - i - 0.5 + (H / 2) + boxMuller()[1];
-                V[2] = Q[2] - (W / (2 * tan(fov / 2))) + boxMuller()[2]; 
-                Vector n = (V - Q) / sqrt(dot(V - Q, V - Q));     
-
+                Vector M = boxMuller();
+                V[0] = Q[0] + 0.5 + j - (W / 2) + M[0];
+                V[1] = Q[1] - i - 0.5 + (H / 2) + M[1];
+                Vector n = (V - Q) / sqrt(dot(V - Q, V - Q));                       //normalized ray direction
                 Vector color = scene.get_color(Ray(Q, n), 5, light_source);
                 sum += color;
             }
@@ -53,6 +52,6 @@ int main() {
             img[(i*W+j)*3 + 2] = min(255.,max(0., pow(sum[2], power)*255));
         }
     }
-    stbi_write_png("test.png", W, H, 3, &img[0], 0);
+    stbi_write_png("antialising_50.png", W, H, 3, &img[0], 0);
     return 0;
 }
