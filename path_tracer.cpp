022578ad301b091a5c22d694cb1c95a764c3601e
mariabrbz <maria.barbulescu@polytechnique.edu>
2020-05-03 09:20:06 +0200

    test

--- path_tracer.cpp
+++ path_tracer.cpp
@@ -14,8 +14,8 @@ int main() {
     Sphere cyan_sphere(Vector(1000, 0, 0), 940, Vector(0, 1, 1), "diffuse");
     Sphere yellow_sphere(Vector(-1000, 0, 0), 940, Vector(1, 1, 0), "diffuse");
     Sphere object(Vector(0, 0, 0), 10, Vector(1, 1, 1), "transparent");
-    Sphere object2(Vector(20.5, 0, 0), 10, Vector(1, 1, 1), "transparent");
-    Sphere object3(Vector(-20.5, 0, 0), 10, Vector(1, 1, 1), "mirror");
+    //Sphere object2(Vector(20.5, 0, 0), 10, Vector(1, 1, 1), "transparent");
+    //Sphere object3(Vector(-20.5, 0, 0), 10, Vector(1, 1, 1), "mirror");
     Sphere light_source(Vector(-10, 25, -10), 5, Vector(1, 1, 1), "light");
 
     //creating the scene
@@ -28,7 +28,7 @@ int main() {
     double W = 600;                             //grid width
     double H = 512;                             //grid height
     double fov = PI/2;                        //alpha, field of view
-    int limit = 6000;                            //amount of rays
+    int limit = 2000;                            //amount of rays
     vector<unsigned char> img(W*H*3);           //image vector
 
     #pragma omp parallel for
